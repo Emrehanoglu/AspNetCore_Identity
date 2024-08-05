2edb0524bdd0542eb70d0be40cc6856e76d956fc
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-08-05 21:50:28 +0300

    Identity #2

---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel(string userName, string email, string phone, string password, string passwordConfirm)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+            PasswordConfirm = passwordConfirm;
+        }
+
+        [Display(Name = "Kullanıcı Adı :")]
+        public string UserName { get; set; }
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+        [Display(Name = "Telefon :")]
+        public string Phone { get; set; }
+        [Display(Name = "Şifre :")]
+        public string Password { get; set; }
+        [Display(Name = "Şifre Tekrar :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
