b8fc4ae8413ae4329ea6c96aaed8d482e37dcdb2
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-08-06 04:59:53 +0300

    Identity #5

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AspNetCoreIdentityApp.Web.Areas.Admin.Models;
+using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Home")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("UserList")]
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+
+            var userViewModelList = userList.Select(x => new UserViewModel
+            {
+                Id = x.Id,
+                Name = x.UserName,
+                Email = x.Email
+            });
+
+            return View(userViewModelList.ToList());
+        }
+    }
+}
