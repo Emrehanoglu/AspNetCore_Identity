94c29a669b2d50ec3b07b6b25fda4294d5c2b6f8
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-08-20 09:00:12 +0300

    Identity #49

---
+++ AspNetCoreIdentityApp.Web/Requirements/ExchangeExpireRequirement.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentityApp.Web.Requirements
+{
+    public class ExchangeExpireRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class ExchangeExpireRequirementHandler : AuthorizationHandler<ExchangeExpireRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExchangeExpireRequirement requirement)
+        {
+            if (!context.User.HasClaim(x => x.Type == "ExchangeExpireDate"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var exchangeExpireClaim = context.User.Claims.FirstOrDefault(x => x.Type == "ExchangeExpireDate");
+
+            if(DateTime.Now > Convert.ToDateTime(exchangeExpireClaim!.Value))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
